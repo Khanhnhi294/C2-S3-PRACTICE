cd7f9b3871f185efc7aebcdbff7dd202a26aa9a0
unknown <admin@pnvl199.pn.com.vn>
2024-05-05 19:40:18 +0700

    done

---
+++ exercise-1/src/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function headerComponent(){
+    return (
+        <header className="block">
+            <h2>WELCOME TO THIS COURSE!</h2>
+        </header>
+    );
+}
+export default headerComponent;
---
+++ exercise-3/src/components/Header.jsx
@@ -0,0 +1,12 @@
+let title = "Atomic Clock"
+
+function Header() {
+    const title = "The amazing atomic clock"; 
+    return(
+        <header>
+            <h1>{title}</h1>
+        </header>
+    );
+}
+
+export default Header;
